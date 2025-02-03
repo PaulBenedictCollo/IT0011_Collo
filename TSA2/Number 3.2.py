2e8498da76ccd94e61f069f55793debd4f96b8a9
PaulBenedictCollo <paulbenedictcollo@gmail.com>
2025-02-03 10:52:23 +0800

    COMMITS

--- TSA2/Number 3.2.py
+++
@@ -1,9 +0,0 @@
-first_name = first_name = input("Please Enter First Name: ")
-last_name = input("Please Enter Last Name: ")
-full_name = first_name + " " + last_name 
-
-print("Uppercase Full Name: ", full_name.upper())
-print("Uppercase Full Name: ", full_name.lower())
-
-name_length = len(full_name)
-print("Length of Full Name: ", name_length)
